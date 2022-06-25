6e3ad433b80b5dacdf2ed3f2c78aebe85bc0ebb3
VR-25 <28943176+VR-25@users.noreply.github.com>
2022-06-25 22:16:33 +0100

    Optimize memory usage

--- install/logf.sh
+++ install/logf.sh
@@ -5,32 +5,35 @@ logf() {
     mkdir -p $dataDir/logs
 
     exec 2>> ${log:-/dev/null}
-    cd $TMPDIR
+    mkdir -p $TMPDIR/.logf
+    cd $TMPDIR/.logf
     set +e
 
     $execDir/power-supply-logger.sh
     { parse_switches 2>/dev/null || ./acca --parse; } > acc-p.txt
 
-    cp ch-switches charging-switches.txt
-    cp oem-custom oem-custom.txt 2>/dev/null
-    cp ch-curr-ctrl-files charging-current-ctrl-files.txt
-    cp ch-volt-ctrl-files charging-voltage-ctrl-files.txt
+    ln -f ../ch-switches charging-switches.txt
+    ln -f ../oem-custom oem-custom.txt 2>/dev/null
+    ln -f ../ch-curr-ctrl-files charging-current-ctrl-files.txt
+    ln -f ../ch-volt-ctrl-files charging-voltage-ctrl-files.txt
 
     for file in /cache/magisk.log /data/cache/magisk.log; do
-      [ -f $file ] && cp $file ./ && break
+      [ -f $file ] && ln -sf $file ./ && break
     done
 
-    cp $dataDir/logs/* /sdcard/Download/acc-t_*.log ./ 2>/dev/null
+    ln -sf $dataDir/logs/* /sdcard/Download/acc-t_*.log ./ 2>/dev/null
     grep -Ev '^#|^$' $config_ > ./config.txt
     set +x
 
     . $execDir/batt-info.sh
     (cd /sys/class/power_supply/
-    batt_info > $TMPDIR/acc-i.txt)
+    batt_info > $TMPDIR/.logf/acc-i.txt)
     dumpsys battery > dumpsys-battery.txt
+    ln -f ../*.txt ../*.log ./ 2>/dev/null
 
-    tar -c *.log *.txt | gzip -9 > $dataDir/logs/acc-logs-$device.tgz
-    rm *.txt magisk.log in*.log power*.log 2>/dev/null
+    tar -hc *.log *.txt | gzip -9 > $dataDir/logs/acc-logs-$device.tgz
+    rm -rf $TMPDIR/.logf 2>/dev/null
+
     cp -f $dataDir/logs/acc-logs-$device.tgz /sdcard/Download/ 2>/dev/null \
       && echo /sdcard/Download/acc-logs-$device.tgz \
       || echo $dataDir/logs/acc-logs-$device.tgz
