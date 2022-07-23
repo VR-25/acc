7d5f89970e9ef7a2c1a7814db452f65b3310076a
VR-25 <28943176+VR-25@users.noreply.github.com>
2022-07-23 09:28:10 +0100

    Add adb push helper

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# Push and Install Zip
+# Copyright 2022, VR25
+# License: GPLv3+
+#
+# usage: $0 [adb device]
+
+id=$(sed -n "s/^id=//p" module.prop)
+version="$(sed -n 1p changelog.md | sed 's/[*()]//g')"
+versionCode=${version#* }
+version=${version% *}
+zip=${id}_${version}_$versionCode
+zip=$(echo _builds/$zip/$zip*zip)
+dest=/sdcard/Download/${zip##*/}
+
+adb $([ -z "${1-}" ] || echo "-s $1") push $zip $dest \
+  && adb $([ -z "${1-}" ] || echo "-s $1") shell su -c magisk --install-module $dest || :
