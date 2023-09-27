a4e85580405fdfaa9240790736b5bbd558e504e8
VR-25 <28943176+VR-25@users.noreply.github.com>
2023-09-27 17:07:26 +0100

    Add `google,charger/charge_start_level`

--- install/ctrl-files.sh
+++ install/ctrl-files.sh
@@ -58,6 +58,7 @@ battery/input_suspend 0 1 /proc/mtk_battery_cmd/en_power_path 1 1
 /sys/class/qcom-battery/restricted_charging 0 1
 /sys/devices/platform/battery_meter/FG_suspend_current_threshold 100 battery/capacity
 /sys/devices/platform/battery_meter/FG_suspend_current_threshold 100 5
+/sys/devices/platform/google,charger/charge_start_level 0 1
 /sys/devices/platform/google,charger/charge_stop_level 100 battery/capacity
 /sys/devices/platform/google,charger/charge_stop_level 100 5
 /sys/kernel/debug/google_charger/chg_mode 0 1
