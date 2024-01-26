07441047784f5f62aa676aa2ea45bced77200928
VR-25 <28943176+VR-25@users.noreply.github.com>
2024-01-26 18:55:38 +0100

    [acc -t] Add status column hint

--- install/batt-interface.sh
+++ install/batt-interface.sh
@@ -117,7 +117,7 @@ status() {
     [ $_status = Idle ] || idle_discharging
   fi
 
-  [ -z "${exitCode_-}" ] || echo -e "  switch: ${switch:--} (${swValue:-N/A})\tcurrent: $(calc $curNow \* 1000 / ${ampFactor:-$ampFactor_} | xargs printf %.f)mA ($_status)"
+  [ -z "${exitCode_-}" ] || echo -e "  switch: ${switch:--} (${swValue:-N/A})\tcurrent: $(calc $curNow \* 1000 / ${ampFactor:-$ampFactor_} | xargs printf %.f)mA\tstatus: $_status"
 
   for i in Discharging DischargingDischarging Idle IdleIdle; do
     [ $i != ${1-}$_status ] || return 0
